d2d02355800928b7030547c13866a2eafccfc077
muzneerturk-creator <muzneerturk@gmail.com>
2025-12-23 15:44:13 +0300

    Update test_login_success.py

--- selenium-tests/test_login_success.py
+++ selenium-tests/test_login_success.py
@@ -2,12 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 def test_valid_login():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new") 
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
     driver.get("https://the-internet.herokuapp.com/login")
 
