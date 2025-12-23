5c5b7a9840c271a2ef17b1df07dfec7ddc451fec
muzneerturk-creator <muzneerturk@gmail.com>
2025-12-23 13:21:05 +0300

    Update test_login_success.py

--- selenium-tests/test_login_success.py
+++ selenium-tests/test_login_success.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 def test_valid_login():
-    driver = webdriver.Chrome()
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+
     driver.get("https://the-internet.herokuapp.com/login")
 
     username = driver.find_element(By.ID, "username")
